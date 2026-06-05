eb894f849a747c980282e0c0c2fec9729466e845
forbigbang81-netizen <forbigbang81@gmail.com>
2026-06-05 17:43:40 -0500

    Update DubSubToggle.tsx

--- components/DubSubToggle.tsx
+++ components/DubSubToggle.tsx
@@ -1,1 +1,33 @@
-.
+"use client";
+
+import { useState } from "react";
+
+export default function DubSubToggle() {
+  const [dub, setDub] = useState(true);
+
+  return (
+    <div className="flex gap-2">
+      <button
+        onClick={() => setDub(true)}
+        className={
+          dub
+            ? "bg-purple-600 px-4 py-2 rounded-lg"
+            : "bg-zinc-700 px-4 py-2 rounded-lg"
+        }
+      >
+        Dub
+      </button>
+
+      <button
+        onClick={() => setDub(false)}
+        className={
+          !dub
+            ? "bg-purple-600 px-4 py-2 rounded-lg"
+            : "bg-zinc-700 px-4 py-2 rounded-lg"
+        }
+      >
+        Sub
+      </button>
+    </div>
+  );
+}
