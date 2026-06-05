f8fab25afb4ece52fd272ec7dd5d78a6c25cc24a
forbigbang81-netizen <forbigbang81@gmail.com>
2026-06-05 17:44:42 -0500

    Update page.tsx

--- app/anime/[id]/schedule/downloads/page.tsx
+++ app/anime/[id]/schedule/downloads/page.tsx
@@ -1,1 +1,9 @@
-yest
+export default function DownloadsPage() {
+  return (
+    <main className="bg-[#080510] min-h-screen p-8 text-white">
+      <h1 className="text-4xl font-bold">
+        Downloads
+      </h1>
+    </main>
+  );
+}
