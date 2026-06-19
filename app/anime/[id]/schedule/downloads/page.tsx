9cfc7e982376a059fa339fc7bdd355a69832aa54
forbigbang81-netizen <forbigbang81@gmail.com>
2026-06-18 23:04:58 -0500

    Delete app directory

--- app/anime/[id]/schedule/downloads/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function DownloadsPage() {
-  return (
-    <main className="bg-[#080510] min-h-screen p-8 text-white">
-      <h1 className="text-4xl font-bold">
-        Downloads
-      </h1>
-    </main>
-  );
-}
--- app/anime/[id]/schedule/downloads/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function ProfilePage() {
-  return (
-    <main className="bg-[#080510] min-h-screen p-8 text-white">
-      <h1 className="text-4xl font-bold">
-        Profile
-      </h1>
-    </main>
-  );
-}
